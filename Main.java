e34147d91496f26e6899018b8d29c52a11ca4b34
ESTEVAO DA <109827306+estevaosbr@users.noreply.github.com>
2022-08-13 02:21:01 +0000

    .

--- Main.java
+++ Main.java
@@ -15,12 +15,30 @@ public class Main {
       // System.out.println(g1.complement());
 
       Graph g1 = new Graph(4);
-      g1.addEdge(0,1,1);g1.addEdge(0,3,1);g1.addEdge(1,0,1);g1.addEdge(3,0,1);
+      g1.addEdge(0,1,1);g1.addEdge(0,3,1);
+      g1.addEdge(1,0,1);g1.addEdge(3,0,1);
       System.out.println(g1);
+      
       Graph g2 = new Graph(4);
-      g2.addEdge(0,2,1);g2.addEdge(2,0,1);g2.addEdge(1,2,1);g2.addEdge(2,1,1);
+      g2.addEdge(0,2,1);g2.addEdge(2,0,1);
+      g2.addEdge(1,2,1);g2.addEdge(2,1,1);
+      g2.addEdge(2,3,1);g2.addEdge(3,2,1);
+      g2.addEdge(1,3,1);g2.addEdge(3,1,1);
+      System.out.println(g2);
+      
       Graph g3 = new Graph(4);
+      g3.addEdge(0,3,1);g3.addEdge(3,0,1);
+      System.out.println(g3);
+      
       Graph g4 = new Graph(4);
+      g4.addEdge(0,1,1);g4.addEdge(1,0,1);
+      g4.addEdge(2,3,1);g4.addEdge(3,2,1);
+      System.out.println(g4);
+
+      System.out.println(g1.subgraph(g2));
+      System.out.println(g1.subgraph(g3));
+      System.out.println(g2.subgraph(g3));
+      System.out.println(g1.subgraph(g4));
       
 
       
