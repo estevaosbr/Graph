e5396e91c0db2efe9e09006babc34db61cacea26
estev <estevao.pinto@aluno.ufop.edu.br>
2022-10-28 13:07:40 -0300

    28/10

--- Main.java
+++ Main.java
@@ -2,12 +2,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
-        GraphMatrix graphMatrix = new GraphMatrix("cm\\facebook_combined.txt");
-        graphMatrix.bellmanFord(0,2620);
 
-        GraphList graphList = new GraphList("cm\\facebook_combined.txt");
-        graphList.bellmanFord(0,2620);
+        Labirinto labirinto = new Labirinto("maze\\maze50.txt");
 
     }
 }
